2e23e04b8cb4e677d65cd905d5e72e8d5d792ddb
Matheus <matheusvsn2005@gmail.com>
2023-04-18 11:38:04 -0300

    added navbar with dark/light toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        "dark-blue": "hsl(209, 23%, 22%)",
+        "very-dark-blue": "hsl(207, 26%, 17%)",
+        "very-dark-desaturated-blue": "hsl(200, 15%, 8%)",
+        "dark-grayish-blue": "hsl(0, 0%, 52%)",
+        "light-grayish-blue": "hsl(0, 0%, 98%)",
+      }
     },
   },
   plugins: [],
-}
+};
