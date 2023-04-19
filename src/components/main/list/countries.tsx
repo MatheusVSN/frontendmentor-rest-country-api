7662229235c06b34cc89355861fc211836c94e23
Matheus <matheusvsn2005@gmail.com>
2023-04-18 21:49:29 -0300

    initial setup for responsive approach

--- src/components/main/list/countries.tsx
+++ src/components/main/list/countries.tsx
@@ -13,9 +13,9 @@ interface CountryCardProps {
 function CountryCardComponent({Flag, FlagAlt, Name, Population, Region, Capital} : CountryCardProps) {
     return (
         <div className="flex flex-col">
-            <Image className="rounded-t-md" src={Flag} alt={FlagAlt} width={300} height={200} />
+            <Image className="rounded-t-md w-full h-[200px] object-cover" src={Flag} alt={FlagAlt} width={300} height={200} />
             <div className="bg-white dark:bg-very-dark-blue rounded-b-md p-8 gap-y-4">
-                <h2 className="font-bold text-xl mb-4">{Name}</h2>
+                <h2 className="font-bold text-xl mb-4 w-full">{Name}</h2>
                 <p><b className="font-semibold">Population: </b>{Population}</p>
                 <p><b className="font-semibold">Region: </b>{Region}</p>
                 <p><b className="font-semibold">Capital: </b>{Capital}</p>
@@ -30,8 +30,9 @@ export default function CountriesList() {
     if(loading) return <h1>Loading...</h1>
 
     console.log(countries);
+
     return (
-        <div className="mt-8 flex flex-col items-center gap-8">
+        <div className="mt-8 px-8 grid place-content-center md:grid-cols-4 gap-8">
             {countries.slice(0, 8).map(index => {
                 const CountryProperties = {
                     Flag: index.flags.svg,
